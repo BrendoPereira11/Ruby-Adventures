5c673f5adcff7e9ded875e284e13c37667330131
BrendoPereira11 <a62612@alunos.ipb.pt>
2026-03-30 23:49:13 +0000

    Set up a 2D player character
    Set up a 2D player character, a62612

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        transform.position = position;
+    }
+}
